b8c4d0147eebf5d2fd26f705dd75e357f0dd059b
Aleksandr Belozerov <allbel900@gmail.com>
2022-10-14 23:57:56 +0300

    reducer: added app

---
+++ src/store/app-reducer.ts
@@ -0,0 +1,12 @@
+const initState: any = {}
+
+export const appReducer = (state = initState, action: AppReducerActionsType): any => {
+    switch (action.type) {
+        // case 'TYPE':
+        //     return {...state}
+        default:
+            return state
+    }
+}
+
+export type AppReducerActionsType = any
