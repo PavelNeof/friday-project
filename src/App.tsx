93bb5e827a6b62927b1a247b141bfc37a9600a51
PavelNeof <pavelneof@gmail.com>
2022-10-14 21:41:32 +0300

    commit1

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import SuperButton from "./Component/SuperButton/SuperButton";
-import SuperInputText from "./Component/SuperInput/SuperInputText";
-import SuperCheckbox from "./Component/SuperCheckbox/SuperCheckbox";
 import {HashRouter, Routes} from "react-router-dom";
 import Router from "./Route";
 import Header from "./Component/Header/Header";
 
-export const PATH = {
-    LOGIN: '/login',
-    registration: '/registration',
-    error: '/error',
-    MAIN:'/'
-    // add paths
-}
-/*логинизация
-регистрация
-профайл
-404 (можно застилизовать заранее)
-восстановление пароля
-ввод нового пароля
-тестовая - отобразить/продемонстрировать все SuperКопмоненты*/
+
 function App() {
     return (
         <div className="App">
