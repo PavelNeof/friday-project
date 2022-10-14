2875f88759bdc0a05b430abf3b56cfe627372ccc
Aleksandr Belozerov <allbel900@gmail.com>
2022-10-15 00:19:57 +0300

    typing: thunk, useDispatch, useSelector

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,28 @@
-
-import thunk from 'redux-thunk'
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk'
+import {applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
+import {AuthActionsType, authReducer} from "./auth-reducer";
+import {appReducer, AppReducerActionsType} from "./app-reducer";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 
 
 const reducers = combineReducers({
-
-
+    auth: authReducer,
+    app: appReducer,
 })
 
 const store = createStore(reducers, applyMiddleware(thunk))
 
-export default store
+export type AppStateType = ReturnType<typeof store.getState>
+export type AppDispatchType = ThunkDispatch<AppStateType, unknown, AppActionsType>
+export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, AppStateType, unknown, AppActionsType>
 
-export type AppStoreType = ReturnType<typeof reducers>
+export const useAppDispatch = () => useDispatch<AppDispatchType>()
+export const useAppSelector: TypedUseSelectorHook<AppStateType> = useSelector
+
+export type AppActionsType = AuthActionsType
+    | AppReducerActionsType
+
+export default store
 
 // @ts-ignore
 window.store = store // for dev
