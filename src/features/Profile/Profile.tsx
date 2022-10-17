3a43dd8ae478a1ecaed22913bc8129e0be24279a
PavelNeof <pavelneof@gmail.com>
2022-10-17 20:14:53 +0300

    style: Profile.txs

--- src/features/Profile/Profile.tsx
+++ src/features/Profile/Profile.tsx
@@ -1,10 +1,14 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import React from "react";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, NavLink } from "react-router-dom";
 import { AppStateType, useAppDispatch } from "../../layers/bll/store";
 import { logoutTC, updateName } from "../../layers/bll/auth-reducer";
 import { PATH } from "../../common/Routing/Route/Route";
+import { Button, FormControl, Grid } from "@mui/material";
+import styles from "../Registration/Registration.module.css";
+import styleProfile from "./Profile.module.css";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
 
 export const Profile = () => {
     const isLoggedIn = useSelector<AppStateType, boolean>(
@@ -51,34 +55,74 @@ export const Profile = () => {
 
     return (
         <>
-            avatar: {avatar}
-            <div>
-                {!editMode && (
-                    <div>
-                        <span onDoubleClick={activateEditMode}>
-                            name:{currentName || email || "---"}
-                        </span>
+            <Grid container className={styles.container}>
+                <FormControl
+                    className={styles.signUpForm}
+                    style={{ border: "1px white solid" }}
+                >
+                    <div className={styleProfile.profile}>
+                        <h1>Personal Information</h1>
+
+                        <div>avatar: {avatar}</div>
+                        <div>
+                            {!editMode && (
+                                <div>
+                                    <span onDoubleClick={activateEditMode}>
+                                        {currentName || email || "---"}
+                                    </span>
+                                    <span
+                                        style={{ color: "#414141" }}
+                                        onClick={activateEditMode}
+                                    >
+                                        {" "}
+                                        <BorderColorIcon />{" "}
+                                    </span>
+                                </div>
+                            )}
+                            {editMode && (
+                                <div>
+                                    <input
+                                        autoFocus={true}
+                                        onChange={onNameChange}
+                                        onBlur={deactivateEditMode}
+                                        value={currentName}
+                                    />
+                                    <button
+                                        onClick={deactivateEditMode}
+                                        style={{
+                                            backgroundColor: "#366eff",
+                                            color: "white",
+                                            border: "#366eff",
+                                        }}
+                                    >
+                                        SAVE
+                                    </button>
+                                </div>
+                            )}
+                        </div>
+
+                        <span style={{ color: "#868686FF" }}> {email} </span>
+                        <Button
+                            style={{
+                                color: "black",
+                                backgroundColor: "#ffffff",
+                                boxShadow:
+                                    "0px 4px 18px rgba(54, 110, 255, 0.6), inset 0px 1px 0px rgba(255, 255, 255, 0.5)",
+                                borderRadius: "30px",
+                                padding: "5px 25px 5px 25px",
+                                fontFamily: "Montserrat",
+                                textTransform: "capitalize",
+                            }}
+                            disabled={disable}
+                            onClick={() => {
+                                dispatch(logoutTC());
+                            }}
+                        >
+                            LogOut
+                        </Button>
                     </div>
-                )}
-                {editMode && (
-                    <div>
-                        <input
-                            autoFocus={true}
-                            onChange={onNameChange}
-                            onBlur={deactivateEditMode}
-                            value={currentName}
-                        />
-                    </div>
-                )}
-            </div>
-            <button
-                disabled={disable}
-                onClick={() => {
-                    dispatch(logoutTC());
-                }}
-            >
-                LogOut
-            </button>
+                </FormControl>
+            </Grid>
         </>
     );
 };
